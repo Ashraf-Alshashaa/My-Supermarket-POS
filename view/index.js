5297daccab35381d29ed47c279ac7c0ea4507e6c
ashraf alshashaa <ashraf.alchechae@gmail.com>
2023-12-28 19:29:41 +0100

    Created add products form elem

--- view/index.js
+++ view/index.js
@@ -2,3 +2,47 @@ const msg = document.getElementById("msg");
 const sidebar = document.getElementById("sidebar");
 const main = document.getElementById("main");
 const rightSection = document.getElementById("right-section");
+
+const resetForm = (formName) => document.forms[formName].reset();
+const getElem = (id) => document.getElementById(id);
+
+const addProductForm = () => {
+  const element = document.createElement("form");
+  element.id = "add-product-form";
+  element.innerHTML = String.raw`
+  <label>Barcode</label>
+    <input name="barcode" type="text" required>
+    <label>Name</label>
+    <input name="name" type="text" required>
+    <label>Buying Price</label>
+    <input name="buy_price" type="number" required>
+    <label>Selling Price</label>
+    <input name="sell_price" type="number" required>
+    <label>Quantity</label>
+    <input name="qty" type="number" required>
+    <label>Category</label>
+    <input name="category" type="text" required>
+    <div class="add-products-btn-cont">
+      <button id="add-products-btn" type="submit">Add</button>
+      <button id="cancel-products-btn" type="button">Cancel</button>
+    </div>
+  `;
+  return element;
+};
+
+rightSection.appendChild(addProductForm());
+
+const submitForm = (formName, cb) => {
+  let data = {};
+  Array.from(document.forms[formName].getElementsByTagName("input")).map(
+    (input) => (data[input.name] = input.value)
+  );
+  cb(data);
+  data = {};
+  resetForm(formName);
+};
+
+getElem("add-product-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+  submitForm("add-product-form", eel.add_product);
+});
