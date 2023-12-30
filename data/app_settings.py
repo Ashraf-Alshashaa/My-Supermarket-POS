09e401ba04d428b9f1bf059a295c8ded4137eb60
ashraf alshashaa <ashraf.alchechae@gmail.com>
2023-12-30 06:18:09 +0100

    Improver the app settings class

--- data/app_settings.py
+++ data/app_settings.py
@@ -1,16 +1,16 @@
 import json
 import os
 
-ILE_PATH = os.path.join(".", "data", "storage", "app_settings.json")
+FILE_PATH = os.path.join(".", "data", "storage", "app_settings.json")
 class AppSettings:
     
     def __init__(self):
         self.app_settings = self.get_all()
 
     def get_all(self):
-        if os.path.exists(ILE_PATH):
-            with open(ILE_PATH, "r") as r:
-                return json.load(r).get('settings', {})
+        if os.path.exists(FILE_PATH):
+            with open(FILE_PATH, "r") as settings_file:
+                return json.load(settings_file).get('settings', {})
         else:
             return {}
 
@@ -18,8 +18,8 @@ class AppSettings:
         for key, value in settings.items():
             self.app_settings[key] = value
         data = {"settings": self.app_settings}
-        with open(ILE_PATH, "w+") as f:
-            json.dump(data, f)
+        with open(FILE_PATH, "w+") as settings_file:
+            json.dump(data, settings_file)
 
     def get_one(self, setting_name):
         return self.app_settings.get(setting_name, {})
