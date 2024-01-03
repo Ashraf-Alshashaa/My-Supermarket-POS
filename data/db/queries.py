6025af24c1e8e834b069f2516e253873585cba87
ashraf alshashaa <ashraf.alchechae@gmail.com>
2024-01-03 06:34:28 +0100

    Created the create db function

---
+++ data/db/queries.py
@@ -0,0 +1,47 @@
+CREATE_CATEGORIES_TABLE = '''
+                CREATE TABLE IF NOT EXISTS categories (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL
+                )
+            '''
+
+CREATE_PRODUCTS_TABLE = '''
+                CREATE TABLE IF NOT EXISTS products (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    barcode INTEGER,
+                    name TEXT NOT NULL,
+                    img BLOB NOT NULL,
+                    buy_price INTEGER NOT NULL,
+                    sell_price INTEGER NOT NULL,
+                    tax INTEGER NOT NULL,
+                    qty INTEGER NOT NULL,
+                    category INTEGER,
+                    FOREIGN KEY (category) REFERENCES categories(id)
+                )
+            '''
+
+CREATE_SETTING_TABLE = '''
+                CREATE TABLE IF NOT EXISTS setting (
+                    current_page TEXT NOT NULL
+                )
+            '''
+
+CREATE_ORDERS_TABLE = '''
+                CREATE TABLE IF NOT EXISTS orders (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    order_date DATE,
+                    total_price REAL
+                )
+            '''
+
+CREATE_ORDERS_DETAILS_TABLE = '''
+                CREATE TABLE IF NOT EXISTS order_details (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    order_id INTEGER,
+                    product_id INTEGER,
+                    quantity INTEGER,
+                    price REAL,
+                    FOREIGN KEY (order_id) REFERENCES orders(id),
+                    FOREIGN KEY (product_id) REFERENCES products(id)
+                )
+            '''
